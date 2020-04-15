b6a9e57bfaaf5e3f3cd6d5ed4c34612b16d0f209
unknown <assane@192.168.1.95>
2020-04-15 15:53:04 +0000

    suite dynamisation

--- src/app/pages/agent/agent-consommateur/impression/impression.component.ts
+++ src/app/pages/agent/agent-consommateur/impression/impression.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OperationService } from '../../../../services/operationService';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { TransactionsProvider } from '../../../../services/transactions-service';
+import { SessionService } from '../../../../services/sessionService';
 
 @Component({
   selector: 'app-impression',
@@ -8,11 +11,42 @@ import { OperationService } from '../../../../services/operationService';
   styleUrls: ['./impression.component.scss']
 })
 export class ImpressionComponent implements OnInit {
+  error =  false;
+  message =  "";
+  receptionRecu: string;
   dataOperation: any = {};
+  dataUser: any = {};
   
-  constructor (private router: Router, private opPrv: OperationService) {
+  constructor (private spinner: NgxSpinnerService, private router: Router, private opPrv: OperationService, public api: TransactionsProvider, private sessPrv: SessionService) {
     this.dataOperation = this.opPrv.parseOperation();
    }
+
+   affecterDossier() {
+    console.log("numeroDossier", this.receptionRecu)
+    this.error = false;
+    this.spinner.show();
+    this.api.envoyerMail({numeroTransaction: this.dataOperation.numeroTransaction})
+      .subscribe(
+        data => {
+          console.log("consommerTimbre ",data);
+          if(data==null || data.status!=0) {
+            console.log('error  ', data);
+            this.error = true;
+            this.message = data.message;
+          }else {
+            
+            this.router.navigate(['/impression']);
+            
+          }
+        },
+        error => {
+          console.log('error  ', error)
+          this.error = true;
+          this.message = "Erreur server. veuillez reessayer ulterieurement"
+        },
+        () => this.spinner.hide()
+      );
+  }
 
   retour() {
     this.router.navigate(['/dossier-timbre']);
